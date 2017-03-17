42564944ed11d5b2ade8a44bafc627249829e12f
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2017-03-17 19:19:03 -0400

    Set up to use etch for UI

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,10 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "parserOptions": {"sourceType": "module",
-                      "ecmaVersion": 2017},
+                      "ecmaVersion": 2017,
+                      "ecmaFeatures": {"impliedStrict": true,
+                                       "jsx":true}
+                     },
     "rules": {
         "indent": ["error", 2, {"VariableDeclarator": {"var": 2, "let": 2, "const": 3} }],
         "linebreak-style": ["off"],
